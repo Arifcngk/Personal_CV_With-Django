1ec55132fadb1090486554603fdc8c335c6ec9f5
Arifcngk <93188117+Arifcngk@users.noreply.github.com>
2024-06-01 23:01:09 +0300

    feat : creat general settings model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class GeneralSettings(models.Model):
+    name = models.CharField(max_length=254, default='', blank=True)
+    description = models.CharField(max_length=254, default='', blank=True)
+    parameters = models.CharField(max_length=254, default='', blank=True)
+
+    updated_data = models.DateTimeField(
+        auto_now=True,
+        blank=True
+    )
+    created_data = models.DateTimeField(
+        auto_now_add=True,
+        blank=True
+    )
--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,5 @@ from .views import index  # Değişiklik burada
 
 urlpatterns = [
     path('', index, name='index'),
-    path('contact', contact, name='contact'),
+
 ]
